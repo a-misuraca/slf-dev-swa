0b0e9259d17f1bb08d9ac6ac448c9901fcdb6487
Alberto Misuraca <a.misuraca@reply.it>
2024-12-10 11:44:25 +0100

    UF2

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
 
-  const client_id = "a377054a-6449-472a-8972-cf0026b6adcb&nonce=BlvTACfShe";
+  const client_id = "a377054a-6449-472a-8972-cf0026b6adcb&nonce=1T2lCMSZKz";
   const redirect_uri = "https://calm-beach-00fd54803.4.azurestaticapps.net/data";
   const scope = "openid";
   const response_type = "id_token";
@@ -10,6 +10,8 @@ const Home = () => {
   // const code_challenge_method = "S256";
   // const code_challenge ="s22lcbJQQV-NZDMHSDRT7i0_4UJXR81BmqNW0TfHoM8";
 
+  // https://access4guests.ciamlogin.com/access4guests.onmicrosoft.com/oauth2/v2.0/authorize?
+  client_id=a377054a-6449-472a-8972-cf0026b6adcb&nonce=1T2lCMSZKz&redirect_uri=https://calm-beach-00fd54803.4.azurestaticapps.net/data/&scope=openid&response_type=id_token&prompt=login
   const doLogin  = async () => {
     console.log('doLogin');  
     window.location.href =`https://access4guests.ciamlogin.com/access4guests.onmicrosoft.com/oauth2/v2.0/authorize?
