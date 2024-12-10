28c14488a5547aa312bc9f56eb17d563eb91859b
Alberto Misuraca <a.misuraca@reply.it>
2024-12-10 11:45:57 +0100

    UF3

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
   // const code_challenge ="s22lcbJQQV-NZDMHSDRT7i0_4UJXR81BmqNW0TfHoM8";
 
   // https://access4guests.ciamlogin.com/access4guests.onmicrosoft.com/oauth2/v2.0/authorize?
-  client_id=a377054a-6449-472a-8972-cf0026b6adcb&nonce=1T2lCMSZKz&redirect_uri=https://calm-beach-00fd54803.4.azurestaticapps.net/data/&scope=openid&response_type=id_token&prompt=login
+  // client_id=a377054a-6449-472a-8972-cf0026b6adcb&nonce=1T2lCMSZKz&redirect_uri=https://calm-beach-00fd54803.4.azurestaticapps.net/data/&scope=openid&response_type=id_token&prompt=login
   const doLogin  = async () => {
     console.log('doLogin');  
     window.location.href =`https://access4guests.ciamlogin.com/access4guests.onmicrosoft.com/oauth2/v2.0/authorize?
