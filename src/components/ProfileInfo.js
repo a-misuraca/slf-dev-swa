1b4018e4eb70c22943a06cbb5a12ff61d2e066ab
AntonioMaretta96 <a.maretta@reply.it>
2024-12-13 11:20:37 +0100

    Test user flow auth only Client-Side v12

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -7,6 +7,7 @@ const ProfileInfo = () => {
   const [graphData, setGraphData] = useState();
 
   function RequestProfileData() {
+    instance.setActiveAccount(accounts);
     instance
       .acquireTokenSilent({
         ...loginRequest,
--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
 import { msalConfig } from "./authConfig";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 // Default to using the first account if no account is active on page load
-/* if (
+if (
   !msalInstance.getActiveAccount() &&
   msalInstance.getAllAccounts().length > 0
 ) {
   // Account selection logic is app dependent. Adjust as needed for different use cases.
   msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0]);
 }
- */
+
 // Listen for sign-in event and set active account
-/* msalInstance.addEventCallback((event) => {
+msalInstance.addEventCallback((event) => {
   if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
     const account = event.payload.account;
     msalInstance.setActiveAccount(account);
   }
-}); */
+});
 
 ReactDOM.render(
   <React.StrictMode>
