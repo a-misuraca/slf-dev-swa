78e5e8eef12dd5f3f0a5e94ddeff1d7dbeea934a
AntonioMaretta96 <a.maretta@reply.it>
2024-12-16 13:00:14 +0100

    Test user flow auth only Client-Side v15

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
 import { callMsGraph } from "../graph";
